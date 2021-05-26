4d4cf118ab1f9a6def8311c82356c221f161c361
George Roman <george.roman.99@gmail.com>
2021-05-26 10:10:17 +0300

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,117 @@
+import { BigDecimal, ethereum } from "@graphprotocol/graph-ts";
+
+import { Approval, ERC20, Transfer } from "../generated/ERC20/ERC20";
+import {
+  Account,
+  Token,
+  TokenApproval,
+  TokenBalance,
+} from "../generated/schema";
+
+function loadOrCreateAccount(address: string): Account | null {
+  let account = Account.load(address);
+  if (!account) {
+    account = new Account(address);
+    account.save();
+  }
+  return account;
+}
+
+function loadOrCreateToken(event: ethereum.Event): Token | null {
+  let token = Token.load(event.address.toHex());
+  if (!token) {
+    let erc20 = ERC20.bind(event.address);
+
+    let nameResult = erc20.try_name();
+    if (nameResult.reverted) {
+      return null;
+    }
+
+    let symbolResult = erc20.try_symbol();
+    if (symbolResult.reverted) {
+      return null;
+    }
+
+    let decimalsResult = erc20.try_decimals();
+    if (decimalsResult.reverted) {
+      return null;
+    }
+
+    token = new Token(event.address.toHex());
+    token.name = nameResult.value;
+    token.symbol = symbolResult.value;
+    token.decimals = decimalsResult.value;
+    token.save();
+  }
+  return token;
+}
+
+export function handleApproval(event: Approval): void {
+  let token = loadOrCreateToken(event);
+  if (!token) {
+    return;
+  }
+
+  let owner = event.params.owner.toHex();
+  let spender = event.params.spender.toHex();
+  let value = event.params.value.toBigDecimal();
+
+  let ownerAccount = loadOrCreateAccount(owner);
+  let spenderAccount = loadOrCreateAccount(spender);
+
+  if (!ownerAccount || !spenderAccount) {
+    return;
+  }
+
+  let tokenApproval = TokenApproval.load(
+    token.id + "-" + ownerAccount.id + "-" + spenderAccount.id
+  );
+  if (!tokenApproval) {
+    tokenApproval = new TokenApproval(
+      token.id + "-" + ownerAccount.id + "-" + spenderAccount.id
+    );
+    tokenApproval.token = token.id;
+    tokenApproval.ownerAccount = ownerAccount.id;
+    tokenApproval.spenderAccount = spenderAccount.id;
+  }
+  tokenApproval.value = value;
+  tokenApproval.save();
+}
+
+export function handleTransfer(event: Transfer): void {
+  let token = loadOrCreateToken(event);
+  if (!token) {
+    return;
+  }
+
+  let from = event.params.from.toHex();
+  let to = event.params.to.toHex();
+  let value = event.params.value.toBigDecimal();
+
+  let fromAccount = loadOrCreateAccount(from);
+  let toAccount = loadOrCreateAccount(to);
+
+  if (!fromAccount || !toAccount) {
+    return;
+  }
+
+  let fromTokenBalance = TokenBalance.load(token.id + "-" + fromAccount.id);
+  if (!fromTokenBalance) {
+    fromTokenBalance = new TokenBalance(token.id + "-" + fromAccount.id);
+    fromTokenBalance.token = token.id;
+    fromTokenBalance.account = fromAccount.id;
+    fromTokenBalance.value = BigDecimal.fromString("0");
+  }
+  fromTokenBalance.value = fromTokenBalance.value.minus(value);
+  fromTokenBalance.save();
+
+  let toTokenBalance = TokenBalance.load(token.id + "-" + toAccount.id);
+  if (!toTokenBalance) {
+    toTokenBalance = new TokenBalance(token.id + "-" + toAccount.id);
+    toTokenBalance.token = token.id;
+    toTokenBalance.account = toAccount.id;
+    toTokenBalance.value = BigDecimal.fromString("0");
+  }
+  toTokenBalance.value = toTokenBalance.value.plus(value);
+  toTokenBalance.save();
+}
